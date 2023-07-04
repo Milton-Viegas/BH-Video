c750d681b6efc34e96e31aeddd585f5425a1d128
Milton-Viegas <86444588+Milton-Viegas@users.noreply.github.com>
2023-07-04 23:59:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BH-Video
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-5d42jt)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>BH Video</h1>
+      <p>Black Hole Video is a streaming with public dominio</p>
+    </div>
+  );
+}
