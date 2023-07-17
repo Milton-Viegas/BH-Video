8dd7be5eb8a5f1eeb9e2bc70fbb414bedc3742e8
Milton-Viegas <86444588+Milton-Viegas@users.noreply.github.com>
2023-07-17 18:08:58 +0100

    1 file modified, 5 added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>BH Video</h1>
-      <p>Black Hole Video is a streaming with public dominio</p>
+      <h1>VS:VersusStream:</h1>
+      <p>VersusStream is a streaming with public dominio</p>
     </div>
   );
 }
